bb194d76f7c92c420233e4ffa1c8212c78b8824c
GGANGTONG <qkrds0914@gmail.com>
2024-01-31 12:52:12 +0900

    try/catch 삽입, 이력서 상세 조회/수정  api 구현 완료

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -3,6 +3,7 @@ import { prisma } from '../utils/prisma/index.js';
 import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 import authMiddleware from '../middlewares/auth.middleware.js';
+import queryString from 'querystring';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -51,36 +52,48 @@ router.post('/signUp', async (req, res, next) => {
   } catch (error) {
     console.error(error);
     return res.json({
-      message: error.message,
+      message: error.name,
     });
   }
 });
 //로그인 API
 router.post('/signIn', async (req, res, next) => {
-  const { email, password } = req.body; //email, password
-  const user = await prisma.users.findFirst({
-    where: {
-      email,
-      password,
-    },
-  });
-  if (!user) throw new Error('에 해당하는 에러가 발생하였습니다.');
+  try {
+    const { email, password } = req.body; //email, password
+    const user = await prisma.users.findFirst({
+      where: {
+        email,
+        password,
+      },
+    });
+    const userInfo = await prisma.userInfos.findFirst({
+      where: {
+        userId: +user.userId,
+      },
+    });
+    if (!user) throw new Error('에 해당하는 에러가 발생하였습니다.');
 
-  //토큰을 발급하는 API는 로그인 API 하나임. 그렇기 때문에 ACCESSTOKEN은 여기서 발급돼야 함
-  const accessToken = jwt.sign(
-    {
-      userId: user.userId,
-      email: user.email,
-      password: user.password,
-    },
-    ACCESS_TOKEN_SECRET_KEY,
-    { expiresIn: '12h' }
-  );
-  //이거 쿠키 어디다 저장해야 되지?
-  res.cookie('accessToken', accessToken);
-  return res.status(200).json({
-    message: '로그인에 성공하였습니다.',
-  });
+    //토큰을 발급하는 API는 로그인 API 하나임. 그렇기 때문에 ACCESSTOKEN은 여기서 발급돼야 함
+    const accessToken = jwt.sign(
+      {
+        userId: user.userId,
+        email: user.email,
+
+        name: userInfo.name,
+        password: user.password,
+      },
+      ACCESS_TOKEN_SECRET_KEY,
+      { expiresIn: '12h' }
+    );
+    //이거 쿠키 어디다 저장해야 되지?
+    res.cookie('accessToken', accessToken);
+    return res.status(200).json({
+      message: '로그인에 성공하였습니다.',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: error.name });
+  }
 });
 
 //내 정보 조회API
@@ -108,10 +121,105 @@ router.get('/myInfo', authMiddleware, async (req, res, next) => {
     data: user,
   });
 });
+//모든 이력서 목록 조회 API
+router.post('/allResume', authMiddleware, async (req, res, next) => {
+  const allResume = await prisma.resume.findMany({
+    select: {
+      resumeId,
+      title,
+      introduction,
+      author,
+      status,
+      createdAt,
+    },
+  });
+});
+//이력서 상세 조회 API
+router.get('/myResume', authMiddleware, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const resume = await prisma.resume.findFirst({
+      where: {
+        userId: +userId,
+      },
+      select: {
+        resumeId: true,
+        title: true,
+        introduction: true,
+        author: true,
+        status: true,
+        createdAt: true,
+      },
+    });
+    return res.status(200).json({ data: resume });
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ message: error.name });
+  }
+});
+//이력서 생성 API
+router.post('/resume', authMiddleware, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const { name } = req.userInfo;
+    const { title, introduction, author } = req.body;
+
+    const resume = await prisma.resume.create({
+      // where: {
+      //   userId: +userId,
+      // },
+      data: {
+        userId: +userId,
+        title,
+        author: name,
+        introduction,
+      },
+    });
+
+    return res.status(200).json({
+      data: resume.data,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ message: error.name });
+  }
+});
+//이력서 수정 API
+router.put('/resume/:resumeId', authMiddleware, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const { resumeId } = req.params;
+    const { title, introduction, status } = req.body;
+
+    const resume = await prisma.resume.findFirst({
+      where: {
+        userId: +userId,
+        resumeId: +resumeId,
+      },
+    });
+
+    if (!resume) throw new Error('해당하는 이력서가 존재하지 않습니다.');
+
+    const updatedResume = await prisma.resume.update({
+      where: {
+        userId: +userId,
+        resumeId: +resumeId,
+      },
+      data: {
+        title,
+        introduction,
+        status,
+      },
+    });
+    return res.status(200).json({
+      message: '이력서 수정이 완료되었습니다.',
+      updatedData: updatedResume.data,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ message: error.name });
+  }
+});
+//이력서 삭제 API
 
 export default router;
-//모든 이력서 목록 조회 API
-//이력서 상세 조회 API
-//이력서 생성 API
-//이력서 수정 API
-//이력서 삭제 API
